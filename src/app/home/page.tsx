2cdf3bda0f09d3d0e9642d0ff8850938926995a1
Allen Shen <ashen8@jhu.edu>
2024-04-25 17:49:51 -0400

    added postbar

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,13 +2,14 @@
 
 import {Text} from "@mantine/core";
 import {Shell} from "../../components/Shell";
+import {PostBar} from "../../components/PostBar";
 
 
 export default function HomePage() {
 
     return (
         <Shell>
-            <Text>This is the Home Page content.</Text>
+            <PostBar />
         </Shell>
     );
 }
