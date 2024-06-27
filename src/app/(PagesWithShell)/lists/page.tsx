6f2c6fac89fcc1e5885f6ef78ff7051eacd8786b
Allen Shen <ashen8@jhu.edu>
2024-06-26 23:08:29 -0400

    added not found page

---
+++ src/app/(PagesWithShell)/lists/page.tsx
@@ -0,0 +1,13 @@
+
+"use client";
+
+import { Shell } from '../../../components/Shell';
+import Posts from '../../../components/Posts/Posts';
+import {Teko} from "next/dist/compiled/@next/font/dist/google";
+import {Text} from "@mantine/core";
+
+export default function Lists() {
+    return (
+        <Text>Lists Page</Text>
+    );
+}
