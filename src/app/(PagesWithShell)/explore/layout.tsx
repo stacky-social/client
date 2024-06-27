8b4bec7157c402dcce20a154ee8d33792a6c17e0
Yuqi Li <yuqi0809@outlook.com>
2024-06-26 23:00:01 -0400

    fixed shell issue

--- src/app/(PagesWithShell)/explore/layout.tsx
+++ src/app/(PagesWithShell)/explore/layout.tsx
@@ -1,8 +1,8 @@
 // src/app/explore/layout.tsx
 "use client";
 import { ReactNode } from 'react';
-import { Shell } from '../../components/Shell';
-import { DoubleHeader } from '../../components/DoubleHeader';
+import { Shell } from '../../../components/Shell';
+import { DoubleHeader } from '../../../components/DoubleHeader';
 
 export default function ExploreLayout({ children }: { children: ReactNode }) {
   return (
---
+++ src/app/(PagesWithShell)/notifications/page.tsx
@@ -0,0 +1,13 @@
+
+"use client";
+
+import { Shell } from '../../../components/Shell';
+import Posts from '../../../components/Posts/Posts';
+import {Teko} from "next/dist/compiled/@next/font/dist/google";
+import {Text} from "@mantine/core";
+
+export default function Notifications() {
+    return (
+            <Text>Notifications Page</Text>
+    );
+}
