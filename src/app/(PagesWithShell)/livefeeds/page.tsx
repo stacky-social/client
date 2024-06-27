d88b7d8b96a8e98cc3bc22e5cc5ba1672f2da03b
Yuqi Li <yuqi0809@outlook.com>
2024-06-26 23:00:42 -0400

    fixed shell issue

---
+++ src/app/(PagesWithShell)/livefeeds/page.tsx
@@ -0,0 +1,14 @@
+
+"use client";
+
+import { Shell } from '../../../components/Shell';
+import Posts from '../../../components/Posts/Posts';
+import {Teko} from "next/dist/compiled/@next/font/dist/google";
+import {Text} from "@mantine/core";
+
+export default function Livefeeds() {
+    return (
+            <Text>Livefeeds Page</Text>
+
+    );
+}
