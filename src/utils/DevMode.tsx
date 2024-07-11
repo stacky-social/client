b7eb28512c1b121555f446452964071d4b41b1db
Allen Shen <ashen8@jhu.edu>
2024-07-11 15:17:14 -0400

    stacky

--- src/utils/DevMode.tsx
+++ src/utils/DevMode.tsx
@@ -4,5 +4,5 @@
 export const BASE_URL =
     process.env.NEXT_PUBLIC_MODE === "development"
         ? "http://localhost:3000"
-        : "https://www.jhustacky.vercel.app";
+        : "https://jhustacky.vercel.app";
 
