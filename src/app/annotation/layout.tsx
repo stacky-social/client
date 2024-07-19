f34ef46e37f729f1f4c76eef300d07b7a87a432f
Yuqi Li <yuqi0809@outlook.com>
2024-07-19 12:16:05 -0400

    change guideline

--- src/app/annotation/layout.tsx
+++ src/app/annotation/layout.tsx
@@ -22,25 +22,34 @@ export default function AnnotationPageLayout({ children }: { children: ReactNode
             </AppShell.Header>
 
             <AppShell.Main>
-                <Stack >
+                <Stack>
                     <Title order={2}>Annotation Guidelines</Title>
                     <Text>
-                        (1) How relevant is this comment to the focus post?
+                        Read the conversation on the left, ending in the BLUE POST.<br/>
+                        Rank the 8 posts on the right by whether they EXTEND this conversation in a USEFUL way, continuing from the topic of the blue post.
                     </Text>
                     <Text>
-                        (2) In general does this post agree with the focus post, disagree with the focus post, or is it unclear?
+                        That is, which single post should someone read next if they were trying to understand and frame the topic according to their own values?
+                        <br/>
+                        Useful posts may offer additional perspectives, values, predictions, proposals, comparisons, or evidence about the topic of the discussion.
+                        <br/>
+                        Posts that are understandable, engagingly written, or funny are more likely to help readers and convince them to keep reading.
                     </Text>
                     <Text>
-                        (3) Does this post make an explicit argument/claim?
+                        Rank all 8 posts from most to least useful.
+                        <br/>
+                        Drag the red bars to divide them into 3 tiers:
+                        <br/>
+                        * top tier: Reading any ONE of these posts would be a good use of time.
+                    <br/>
+                        * middle tier: These posts are on-topic, but could be counted rather than read.
+                    <br/>
+                        * bottom tier: This post is off-topic.
                     </Text>
                     <Text>
-                        (4) Does this post present explicit evidence/claim to truth?
-                    </Text>
-                    <Text>
-                        (5) Is this post compelling to read (funny, exciting, easy to understand, etc.)?
-                    </Text>
-                    <Text>
-                        (6) Overall, does this post add to a conversation or argument about the topic?
+                        Also, does each of the 8 posts AGREE or DISAGREE with the blue post's main views, claims, evidence, or values?
+                        <br/>
+                        Select "Agrees" or "Disagrees" accordingly. You may select neither or both.
                     </Text>
                 </Stack>
 
