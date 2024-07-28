4f535e225472585379230fa7c7010e310b838a73
Allen Shen <ashen8@jhu.edu>
2024-07-28 14:32:58 -0400

    fixed tabs

--- src/app/(PagesWithStandardShell)/explore/page.tsx
+++ src/app/(PagesWithStandardShell)/explore/page.tsx
@@ -1,35 +1,21 @@
-// src/app/explore/page.tsx
 "use client";
-import { usePathname } from 'next/navigation';
+
 import { useEffect, useState } from 'react';
 import PostPage from './posts/page';
 import HashtagsPage from './hashtags/page';
 import PeoplePage from './people/page';
 import NewsPage from './news/page';
-import {DoubleHeader} from "../../../components/DoubleHeader";
+import { DoubleHeader } from "../../../components/DoubleHeader";
 
 export default function Explore() {
-  const pathname = usePathname();
-  const [activePage, setActivePage] = useState<string | null>(null);
+  const [activePage, setActivePage] = useState<string>('posts'); // Default to 'posts'
 
   useEffect(() => {
-    console.log('Current pathname:', pathname);
-
-    if (pathname.includes('posts')) {
-      setActivePage('posts');
-    } else if (pathname.includes('hashtags')) {
-      setActivePage('hashtags');
-    } else if (pathname.includes('people')) {
-      setActivePage('people');
-    } else if (pathname.includes('news')) {
-      setActivePage('news');
-    } else {
-      setActivePage(null);
-    }
-  }, [pathname]);
+    const path = window.location.pathname.split('/').pop();
+    setActivePage(path || 'posts');
+  }, []);
 
   const renderActivePage = () => {
-
     switch (activePage) {
       case 'posts':
         return <PostPage />;
@@ -40,16 +26,14 @@ export default function Explore() {
       case 'news':
         return <NewsPage />;
       default:
-        return <div>Select a tab to view content</div>;
+        return <PostPage />;
     }
   };
 
   return (
-    <>
-      <DoubleHeader />
+      <>
+        <DoubleHeader active={activePage} setActive={setActivePage} />
         {renderActivePage()}
-    </>
-
-
+      </>
   );
 }
--- src/app/(PagesWithStandardShell)/explore/posts/page.tsx
+++ src/app/(PagesWithStandardShell)/explore/posts/page.tsx
@@ -1,4 +1,4 @@
-// src/app/explore/posts.tsx
+
 "use client";
 
 const PostPage = () => {
